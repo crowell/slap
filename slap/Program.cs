85281aed057ebe529c2f1d9c36bb14707756590e
Jeffrey Crowell <crowell@bu.edu>
2012-03-16 14:00:39 -0400

    some documentation in the code

--- slap/Program.cs
+++ slap/Program.cs
@@ -14,27 +14,39 @@ namespace slap
 {
     class Program
     {
+        /// <summary>
+        /// prints how to use this tool.
+        /// </summary>
         static void printUsage()
         {
-            System.Console.WriteLine("slap.exe <file> <paste_lang> [private] [deletion time in seconds] -- set private to yes for private paste");
+            //prints out how to use. <> is for manditory, [] is for optional
+            System.Console.WriteLine("slap.exe <file> [paste_lang] [private] [deletion time in seconds] -- set private to yes for private paste");
         }
+        /// <summary>
+        /// copies the string given to the clipboard
+        /// </summary>
+        /// <param name="url">the string wished to be copied</param>
         static void copyToClipboard(string url)
         {
             string command = String.Empty;
             if (Environment.OSVersion.Platform == PlatformID.MacOSX)
             {
+                //its a mac.  pbcoby for pasteboard
                 command = "pbcopy";
             }
             else if (Environment.OSVersion.Platform == PlatformID.Unix)
             {
+                //its linux.  assume that you've got X running.
                 command = "xsel --clipboard --input";
             }
             if (command != String.Empty)
             {
+                //for linux and mac, execute the copy to clipboard
                 Process.Start("echo " + url + " | " + command);
             }
             else
             {
+                //its windows, just use the winforms clipboard
                 Clipboard.SetText(url);
             }
             System.Console.WriteLine("uploaded file to " + url + " and copied url to clipboard");
@@ -43,25 +55,33 @@ namespace slap
         [STAThreadAttribute]
         static void Main(string[] args)
         {
-            bool isPrivate = false;
             //required params file and language
-            if (args.Length < 2)
+            if (args.Length < 1)
             {
+                //no inputs. print how to use, then quit
                 printUsage();
                 Environment.Exit(1);
             }
-            string paste_lang = args[1];
+            string paste_lang = "text"; //default to the "text"
+            if (args.Length > 1)
+            {
+                paste_lang = args[1]; //oh, so you wanted a language, we'll take that.
+            }
             //make sure a good file was put
             if (!File.Exists(args[0]))
             {
-                printUsage();
-                Environment.Exit(1);
+                //it wasn't!!??!? 
+                printUsage(); //tell the user what's up
+                Environment.Exit(1); //quit with return code 1
             }
-            string paste_data = String.Empty;
+            string paste_data = String.Empty; //the actual code that you has
             using (StreamReader sr = new StreamReader(args[0]))
             {
-                paste_data = sr.ReadToEnd();
+                paste_data = sr.ReadToEnd(); //read the file 
             }
+            //submit the code with the following api as the guide
+            // http://paste.kde.org/doc/api/
+            //thanks the KDE team for such a great useful tool!
             PostSubmitter post = new PostSubmitter();
             post.Url = "http://paste.kde.org/";
             post.PostItems.Add("paste_data", paste_data);
@@ -70,7 +90,6 @@ namespace slap
             post.PostItems.Add("mode", "xml");
             if (args.Length > 2 && args[2] == "yes")
             {
-                isPrivate = true;
                 post.PostItems.Add("paste_private", "yes");
             }
             int time;
@@ -111,8 +130,12 @@ namespace slap
                     hash = outerNode.InnerText;
                 }
             }
+            //generate the url based on the reponse
             url = "http://paste.kde.org/" + id + "/" + hash;
+            //copy the url to clipboard
             copyToClipboard(url);
+            //and we're done!
+            //that wasn't so bad.
         }
     }
 }
